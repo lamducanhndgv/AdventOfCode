730b60e418b11d0e7a54e86e10877a69d0048b7d
Lâm Đức Anh <lamducanhndgv@gmail.com>
2025-01-04 23:52:42 +0700

    AOC 2024: temporary solution for day 4

--- 2024/day-4/main.py
+++ 2024/day-4/main.py
@@ -16,12 +16,45 @@ def get_input():
     response.raise_for_status()
 
     # Parse data    
-    data = ...
+    data = response.content.decode("utf-8").strip()
+    data = data.split("\n")
     
     return data
 
+def count_xmas_string(input_data, r, c, m, n):
+    """
+    Find the number of Xmas strings in 8 directions
+    """
+    pattern = "XMAS"
+    directions = [(0, 1), (1, 0), (1, 1), (-1, 0), (0, -1), (-1, -1), (1, -1), (-1, 1)]
+    
+    count = 0
+    for dr, dc in directions:
+        c_index = 0 
+        new_r, new_c = r, c
+        while c_index < len(pattern):
+            new_r = new_r + dr
+            new_c = new_c + dc
+            if 0 <= r < m and 0 <= c < n:
+                if input_data[r][c] == pattern[c_index]:
+                    c_index += 1
+                else:
+                    break
+        if c_index == len(pattern) - 1:
+            count += 1
+    
+    return count
+
 def solution():
     input_data = get_input()
+    m, n = len(input_data), len(input_data[0])
+    
+    total = 0
+    for r in range(m):
+        for c in range(n):
+            if input_data[r][c] == 'X':
+                total += count_xmas_string(input_data, r, c, m, n)
+    print(total)
     
     
 if __name__ == "__main__":
