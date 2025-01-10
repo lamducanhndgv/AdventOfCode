df4e47ad0c50b3ba6029424a9250d2bd29c58224
Lâm Đức Anh <lamducanhndgv@gmail.com>
2025-01-10 23:59:51 +0700

    update

--- 2024/day-8/main.py
+++ 2024/day-8/main.py
@@ -10,7 +10,7 @@ from dotenv import load_dotenv
 load_dotenv(dotenv_path="../.env")
 
 def get_input():
-    data_link = "https://adventofcode.com/2024/day/7/input"
+    data_link = "https://adventofcode.com/2024/day/8/input"
     response = requests.get(
         data_link,
         cookies={"session": os.environ.get("COOKIE_SESSION")}
@@ -21,20 +21,12 @@ def get_input():
     data = response.content.decode("utf-8").strip()
     
     data = data.split('\n')
-    
-    parsed_data = []
-    for row in data:
-        test_value, numbers = row.split(": ")
-        numbers = list(map(int, numbers.split(" ")))
-        parsed_data.append((test_value, numbers))
-    
-    return parsed_data
+
+    return data 
 
 
 def solution():
-    input_data = get_input()
-    for d in input_data:
-        print(d)
+    ...
     
 if __name__ == "__main__":
     solution()
